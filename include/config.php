8772833562766c92d014c9b55f771c1a9d7f1d27
Praful Mestha <117851077+prafulmestha@users.noreply.github.com>
2023-04-29 21:07:27 +0530

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    require_once __DIR__."/../my_config.php";
+
+    // db configration
+    $server   = HOSTNAME;
+    $username = USERNAME;
+    $password = PASSWORD;
+    $database = DATABASE;
+       
+    date_default_timezone_set('Asia/Kolkata');
+    $con=mysqli_connect($server,$username,$password,$database) or die ("could not connect to mysql");
+
+    // some global value
+    $DATETIME_FORMAT = "l, M j, Y @ g:ia";
+    $HOSTNAME = BASE_URL;
+    $COMPANY_NAME = "Blood Bank";
+
+    require_once 'function.php';
